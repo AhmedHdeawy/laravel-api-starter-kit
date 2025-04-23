3c9b715707adc7eef35c9dd8d7b5d67676795e93
Ahmed Hdeawy <ahmedhdeawy@gmail.com>
2025-04-23 06:14:27 +0200

    referring the repo to the original repo of the starter

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
-    App\Providers\ServiceClassProvider::class,
-];
